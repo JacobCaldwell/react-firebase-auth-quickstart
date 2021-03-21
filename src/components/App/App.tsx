fdb3490be96144535a523134d8857993208aed5c
JacobCaldwell <jacobscaldwell1@gmail.com>
2021-03-20 21:43:58 -0500

    updated comps css

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,26 +2,39 @@ import React from 'react';
 // import './App.css';
 import { AuthProvider } from "../../context/AuthContext";
 import Login from "../Login";
-import { Container } from 'react-bootstrap';
+import SignUp from "../SignUp";
+
+// import { Container } from 'react-bootstrap';
 import { PrivateRoute } from '../PrivateRoute';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
 
   return (
     <div className="App">
-      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
+      {/* <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}> */}
+      <Router>
         <AuthProvider>
+
+          <Switch>
+            {/* <PrivateRoute path="/app" component={Dashboard} /> */}
+            <Route exact path="/" component={Login} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={SignUp} />
+          </Switch>
 
           {/* <PrivateRoute exact path="/" component={Login}/>           */}
           {/* <div>
             <Login />
           </div> */}
           {/* <InternalComp></InternalComp> */}
-          <Login />
+          {/* <SignUp /> */}
 
 
         </AuthProvider>
-      </Container>
+      </Router>
+      {/* </Container> */}
 
 
 
